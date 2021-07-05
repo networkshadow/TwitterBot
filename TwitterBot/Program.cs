1682315376f8004c0296ae8c307acf9edd9d1089
Logan MacKinnon <loganmackinnon93@gmail.com>
2021-07-05 14:54:30 -0300

    #3 Added list to Main() with StarWarsQuotes for now. Added two threads for autotweeting and autoreplying

--- TwitterBot/Program.cs
+++ TwitterBot/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TweetSharp;
 using TwitterBot.Tweets;
@@ -11,11 +12,80 @@ namespace TwitterBot
 {
     class Program
     {
+        private static List<string> starWarsQuotes = new List<string>
+        {
+            "Try not. Do or do not. There is no try.",
+            "Your eyes can deceive you; don't trust them.",
+            "Who's the more foolish: the fool or the fool who follows him?",
+            "Your focus determines your reality.",
+            "No longer certain that one ever does win a war, I am.",
+            "Train yourself to let go of everything you fear to lose."
+        };
         static void Main(string[] args)
         {
+            Banner();
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"<{DateTime.Now}> - Twitter Bot Started");
             Console.ResetColor();
+
+            Thread autoTweet = new Thread(AutoTweet);
+            Thread autoReply = new Thread(AutoReply);
+            autoTweet.Start();
+            autoReply.Start();
+
+            Console.ReadLine();
+        }
+
+        public static void AutoTweet()
+        {
+            var flag = true;
+            var random = new Random();
+            var timeout = new TimeSpan(0, 13, 45);//(hours, minutes, seconds)
+
+            while (flag)
+            {
+                try
+                {
+                    var tweet = new Tweet();
+                    tweet.SendTweet(starWarsQuotes[random.Next(0, starWarsQuotes.Count)]);
+                    Thread.Sleep(timeout);
+                }
+                catch (Exception e)
+                {
+                    flag = false;
+                    Console.WriteLine($"<{DateTime.Now}> - {e.Message}");
+                }                
+            }
+        }
+
+        public static void AutoReply()
+        {
+            var flag = true;
+            var timeout = new TimeSpan(0, 1, 10);//(hours, minutes, seconds)
+
+            while (flag)
+            {
+                try
+                {
+                    var tweet = new Tweet();
+                    tweet.ReplyToTweets();
+                    Thread.Sleep(timeout);
+                }
+                catch(Exception e)
+                {
+                    flag = false;
+                    Console.WriteLine($"<{DateTime.Now}> - {e.Message}");
+                }
+            }
+        }
+
+        public static void Banner()
+        {
+            Console.WriteLine("###############################");
+            Console.WriteLine("########TwitterBot#############");
+            Console.WriteLine("########Version 1.0############");
+            Console.WriteLine("########By: NetworkShadow######");
+            Console.WriteLine("###############################");
         }
     }
 }
--- TwitterBot/Tweets/Tweet.cs
+++ TwitterBot/Tweets/Tweet.cs
@@ -16,6 +16,8 @@ namespace TwitterBot.Tweets
         private static TwitterService service = Config.Service;
         private static int imageID = 0;
 
+        private static List<long> currentIDs = new List<long>();
+
         /// <summary>
         /// Method for sending tweet to twitter account
         /// </summary>
@@ -27,7 +29,7 @@ namespace TwitterBot.Tweets
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"<{DateTime.Now}> - Tweet Send!");
+                    Console.WriteLine($"<{DateTime.Now}> - Tweet Sent: {_status}");
                     Console.ResetColor();
                 }
                 else
@@ -74,6 +76,79 @@ namespace TwitterBot.Tweets
                     imageID++;
                 }
             }
+        }
+
+        /// <summary>
+        /// Replies to tweets when they contain a particular word or phrase.
+        /// </summary>
+        public void ReplyToTweets()
+        {
+            var count = 0;
+            List<TwitterStatus> allTweets = GetAllTweetsFromHome(10).ToList();
+            var total = allTweets.Count;
+
+            var findTweetContains = "Trump";
+
+            
+
+            foreach (var tweet in allTweets)
+            {
+                if (tweet.Text.Contains(findTweetContains))
+                {
+                    var isValidToReply = true;
+                    foreach(var id in currentIDs)
+                    {
+                        if(id == tweet.Id)
+                        {
+                            isValidToReply = false;
+                        }
+                    }
+
+                    if (isValidToReply)
+                    {
+                        var tweetString = "What a clown that man is. (Sent Courtesy an Automated Reply Bot searching for Tweets with T*U*P)";
+
+                        service.SendTweet(new SendTweetOptions
+                        {
+                            Status = tweetString,
+                            InReplyToStatusId = tweet.Id
+                        });
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"<{DateTime.Now}> - Replying to Tweet: {tweet.Text}");
+                        Console.WriteLine($"<{DateTime.Now}> - Replied with: {tweetString}");
+                        Console.ResetColor();
+                        count--;
+                    }
+
+                    currentIDs.Add(tweet.Id);
+                }
+
+                count++;
+            }
+
+            if(count >= total)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"<{DateTime.Now}> - No tweets found with '{findTweetContains}'");
+                Console.ResetColor();
+            }
+
+            allTweets.Clear();
+        }
+
+        /// <summary>
+        /// Collects all tweets on the user's home page
+        /// </summary>
+        /// <returns>Tweets from user's home page</returns>
+        private List<TwitterStatus> GetAllTweetsFromHome(int numTweets)
+        {
+            var currentTweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions
+            {
+                Count = numTweets
+            }).ToList();
+
+            return currentTweets;
         }
     }
 }
