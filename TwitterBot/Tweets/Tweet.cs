e3a8f072cc6a367872c03e335ad25aa1dee8f8dc
Logan MacKinnon <loganmackinnon93@gmail.com>
2021-07-05 11:47:52 -0300

    #3 added method for sending tweet with image

--- TwitterBot/Tweets/Tweet.cs
+++ TwitterBot/Tweets/Tweet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,6 +14,7 @@ namespace TwitterBot.Tweets
     {
         //Initialize service
         private static TwitterService service = Config.Service;
+        private static int imageID = 0;
 
         /// <summary>
         /// Method for sending tweet to twitter account
@@ -39,12 +41,39 @@ namespace TwitterBot.Tweets
 
         /// <summary>
         /// Method to allow class to send tweet with an associated image
+        /// 
+        /// User must pass a list of the images' paths from wherever they are
+        /// being currently stored on their local system
+        /// 
         /// </summary>
         /// <param name="_status"></param>
         /// <param name="imageID"></param>
-        public void SendTweetWithMedia(string _status, int imageID)
+        public void SendTweetWithMedia(string _status, List<string> imagesList)
         {
+            using(var stream = new FileStream(imagesList[imageID], FileMode.Open))
+            {
+                service.SendTweetWithMedia(new SendTweetWithMediaOptions
+                {
+                    Status = _status,
+                    Images = new Dictionary<string, Stream> { { imagesList[imageID], stream} }
+                });
 
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"<{DateTime.Now}> - Tweet Sent!");
+                Console.ResetColor();
+
+
+                if ((imageID + 1) == imagesList.Count)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"<BOT> - End of Image Array");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    imageID++;
+                }
+            }
         }
     }
 }
